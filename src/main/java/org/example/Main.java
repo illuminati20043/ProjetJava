f53321ea115a99f4e1ba1b9d3c758c8d576e6f3e
adamguizaoui <adam.guizaoui@wiiisdom.com>
2024-11-12 09:07:35 +0100

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.printf("Bienvenue sur le projet de Adam GUIZAOUI et Elyes AÏSSAT");
+    }
+}
